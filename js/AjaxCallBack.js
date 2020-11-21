f0109762b951cace449e8b1654ed797f60cde8e8
leena0203 <leenasarode02@gmail.com>
2020-11-22 04:05:40 +0530

    Added: to view Employee data from JSON Server and ability to delete and save using promise

--- js/AjaxCallBack.js
+++ js/AjaxCallBack.js
@@ -4,16 +4,21 @@ function showTime(){
     return date.getHours() + "Hrs:" + date.getMinutes() + "Mins:" + date.getSeconds() + "Secs";
     
 }
-function makeAJAXCall(methodType, url, callback, async = true, data = null) {
+function makePromiseCall(methodType, url, async = true, data = null) {
+    return new Promise(function (resolve, reject) {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
         //console.log("State Change called. Ready State : " + xhr.readyState + " Status :" + xhr.status);
         if(xhr.readyState === 4){
             if(xhr.status === 200 || xhr.status === 201){
-                callback(xhr.responseText);
+                resolve(xhr.responseText);
             }
             else if(xhr.status >= 400){
-                console.log("Handle 400 Client Error or 500 Server Error");
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+                console.log("XHR Failed");
             }
         }
     }
@@ -25,25 +30,32 @@ function makeAJAXCall(methodType, url, callback, async = true, data = null) {
     }
     else xhr.send();
     console.log(methodType + " request sent to the server");
-    
+    }); 
 }
 
-const getURL = "http://127.0.0.1:3000/employees/1";
-function getUserDetails(data){
-    console.log("Get User Data at : "+showTime()+"data:" + data)
-}
-makeAJAXCall("GET", getURL, getUserDetails);
-console.log("Made GET AJAX CALL TO Server at "+showTime());
+const getURL = "http://127.0.0.1:3000/employees/2";
+makePromiseCall("GET", getURL, true)
+    .then(responseText => {
+        console.log("Get User data: " + responseText)
+    })
+    .catch(error => console.log("Get Error Status : " + JSON.stringify(error)));
+
 
 const deleteURL = "http://localhost:3000/employees/4";
-function userDeleted(data){
-    console.log("User Deleted " + data);
-}
-makeAJAXCall("DELETE", deleteURL, userDeleted, false);
+makePromiseCall("DELETE", deleteURL, false)
+    .then(responseText => {
+        console.log("User Deleted :  " + responseText)
+    })
+    .catch(error => console.log("Delete Error Status: " + JSON.stringify(error)));
+
+
 const postURL = "http://localhost:3000/employees";
 const emplData = {"name":"Harry","salary":"5000"};
 function userAdded(data){
     console.log("User Added :" + data);
 }
-makeAJAXCall("POST", postURL, userAdded, true, emplData);
-
+makePromiseCall("POST", postURL, true, emplData)
+    .then(responseText => {
+        console.log("User Added :  " + responseText)
+    })
+    .catch(error => console.log("Post Error Status: " + JSON.stringify(error)));
